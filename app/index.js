4313efda8920f11dfafce1787500f16c4821a437
changlijie <clj0147@gewoo.cn>
2017-07-19 15:47:20 +0800

    fixbug

--- app/index.js
+++ app/index.js
@@ -3,6 +3,5 @@ import ReactDOM from "react-dom";
 import Todos from "./components/main";
 import './index.scss';
 
-// ReactDOM.render(< Todos / >, document.getElementById("content"));
 ReactDOM.render(
   <Todos/>, document.getElementById('root'))
